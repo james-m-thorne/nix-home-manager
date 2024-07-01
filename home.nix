785d4d27b6dbb825ec701f67357b31c7ef74c1b5
jmthorne101@gmail.com <jamesthorne@canva.com>
2024-07-01 10:01:14 +1000

    feat: remove vi mode

--- home.nix
+++ home.nix
@@ -91,9 +91,6 @@
           name = "romkatv/powerlevel10k"; 
           tags = [ as:theme depth:1 ]; 
         } # Installations with additional options. For the list of options, please refer to Zplug README.
-        {
-          name = "jeffreytse/zsh-vi-mode";
-        }
       ];
     };
     initExtraFirst = ''
